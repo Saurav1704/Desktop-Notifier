e6146749e7f746fffd0a5fdc034a7593bf44b8be
Saurav1704 <66158077+Saurav1704@users.noreply.github.com>
2021-10-31 01:26:00 +0530

    Add files via upload

---
+++ Saurav.py
@@ -0,0 +1,10 @@
+from plyer import notification
+
+notification.notify(
+title = "Hello Mates",
+message = "The Entreprenuership Network ",
+app_name = "SAurav",
+timeout = 30,
+ticker = "New Notification",
+toast = False
+)
